110ae7a77ddb57e64f13de5d2c78116d339bd36a
Shivam Sahu <51310713+seevam@users.noreply.github.com>
2025-12-14 18:58:34 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhooks(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
 
   if (!isPublicRoute(req)) {
 
